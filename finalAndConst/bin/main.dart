f606fc7abf306a1caffe0f9b03d2e9d3546ebe98
SmartLearn <vivocoder@gmail.com>
2018-09-13 13:11:18 +0300

    final

---
+++ finalAndConst/bin/main.dart
@@ -0,0 +1,11 @@
+
+main(){
+
+  //declaring final variable
+  final name = 'Alex';
+  final String nickname = 'Alexy';
+
+  //you cant change the value of a final variable
+  //name = 'David' ;
+
+}
---
+++ finalAndConst/test/finalAndConst_test.dart
@@ -0,0 +1,8 @@
+import 'package:finalAndConst/finalAndConst.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
